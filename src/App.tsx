5450f9642dd00d391d08d5dcc63ca542a8763e49
9182092380 <pothulasharath452@gmail.com>
2024-10-23 15:23:02 +0530

    file added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import POForm from './components/POForm'; // Import the POForm component
+import './styles/app.css'; 
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Purchase Order Generator</h1>
+      <POForm />
     </div>
   );
-}
+};
 
 export default App;
